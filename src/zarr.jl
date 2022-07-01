1d11fc7e49a2e412b298d661f91c0f81ee445d75
Fabian Gans <fgans@bgc-jena.mpg.de>
2022-07-01 11:04:28 +0200

    promote fillvalues

--- src/zarr.jl
+++ src/zarr.jl
@@ -6,14 +6,14 @@ struct DiskArrayShuffleStore{S<:DiskArrayShuffler} <: AbstractStore
     metadata::Dict{String,Vector{UInt8}}
 end
 Base.show(io::IO,s::DiskArrayShuffleStore) = print(io,"Shuffled Store")s
-function DiskArrayShuffleStore(s::DiskArrayShuffler; name = "batches", attrs = Dict())
+function DiskArrayShuffleStore(s::DiskArrayShuffler; name = "batches", attrs = Dict(),fill_value=nothing)
     onear = s.currentcoll[].chunkviews |> first |> first
     soutput = size(onear)
     cs = (soutput..., s.sampler.batchsize)
     si = (soutput..., s.sampler.batchsize)
     #Create a mock Zarr array to contain the metadata
     g = zgroup(Zarr.DictStore())
-    zcreate(eltype(onear),g,name,si...,chunks = cs,compressor=Zarr.NoCompressor(), attrs=attrs)
+    zcreate(eltype(onear),g,name,si...,chunks = cs,compressor=Zarr.NoCompressor(), attrs=attrs,fill_value=fill_value)
     Zarr.consolidate_metadata(g)
     DiskArrayShuffleStore(s,name,g.storage.a)
 end
